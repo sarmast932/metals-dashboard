7e668fd908b02138ae28db6e13198efb4a70f5df
telnahas05-coder <telnahas05@gmail.com>
2026-02-23 19:43:37 +0330

    switch to official harisma price

--- api/price-check.js
+++ api/price-check.js
@@ -1,50 +1,38 @@
-const { getAllAlerts } = require("../lib/alert-registry");
-const { evaluateAlert } = require("../lib/alert-engine");
-const { savePriceSnapshot } = require("../lib/history");
-
-// نسخه تستی با نوسان تصادفی کوچک
-async function getPrices() {
-  const baseGold = 32000000;
-
-  // نوسان ±0.2٪
-  const randomFactor = 1 + (Math.random() - 0.5) * 0.004;
-
-  return {
-    gold: Math.floor(baseGold * randomFactor),
-    silver: 450000
-  };
-}
+const { getAllAlerts } = require("../lib/alert-registry")
+const { evaluateAlert } = require("../lib/alert-engine")
+const { savePriceSnapshot } = require("../lib/history")
+const { fetchPrices } = require("../lib/price-service")
 
 module.exports = async function handler(req, res) {
   try {
-    const prices = await getPrices();
+    const prices = await fetchPrices()
 
     for (const asset of Object.keys(prices)) {
-      await savePriceSnapshot(asset, prices[asset]);
+      await savePriceSnapshot(asset, prices[asset])
     }
 
-    const alerts = await getAllAlerts();
-    let triggeredCount = 0;
+    const alerts = await getAllAlerts()
+    let triggeredCount = 0
 
     for (const alert of alerts) {
-      const currentPrice = prices[alert.asset];
-      if (!currentPrice) continue;
+      const currentPrice = prices[alert.asset]
+      if (!currentPrice) continue
 
-      const isTriggered = await evaluateAlert(alert, currentPrice);
-      if (isTriggered) triggeredCount++;
+      const isTriggered = await evaluateAlert(alert, currentPrice)
+      if (isTriggered) triggeredCount++
     }
 
     return res.status(200).json({
       success: true,
       prices,
       triggered: triggeredCount
-    });
+    })
 
   } catch (error) {
-    console.error("price-check error:", error);
+    console.error("price-check error:", error.message)
     return res.status(500).json({
       success: false,
       error: error.message
-    });
+    })
   }
-};
+}
